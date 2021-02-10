4c32ec6d2718f85d6b52c7d5cfc61aaf04e2016d
Thiago Monteiro da Silva <thiagom.silva195@gmail.com>
2021-02-09 21:42:07 -0300

    Tratativas inseridas no campo CEP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,32 @@ export class AppComponent {
   logradouro: string = ''
   uf: string = '';
 
-  constructor(public http: HttpClient){
+  constructor(public http: HttpClient) {
 
   }
 
   //Função que busca e retorna o CEP
-  buscaCep(){
-   return this.http.get('https://viacep.com.br/ws/' + this.cep + '/json/').subscribe((data) => {
-    console.log(data);
-    this.bairro = data['bairro'];
-    this.localidade = data['localidade'];
-    this.logradouro = data['logradouro'];
-    this.uf = data['uf'];
-   })
+  buscaCep() {
+    if (this.cep) {
+      if (this.cep.length < 8) {
+        window.alert('O CEP deve conter oito números!');
+      } else {
+        return this.http.get('https://viacep.com.br/ws/' + this.cep + '/json/').subscribe((data) => {
+          console.log(data);
+          if (data['erro']) {
+            window.alert('CEP inválido!');
+          } else {
+            this.bairro = data['bairro'];
+            this.localidade = data['localidade'];
+            this.logradouro = data['logradouro'];
+            this.uf = data['uf'];
+          }
+        }, err => {
+          window.alert('Erro ao buscar cep!');
+        });
+      }
+    } else {
+      window.alert('Digite um CEP!');
+    }
   }
 }
